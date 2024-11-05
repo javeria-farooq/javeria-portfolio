47e9ead1a5cc398136609fa77580b2d45f15d8c4
javeria-farooq <gettingeducation6@gmail.com>
2024-11-05 21:54:41 +0500

    After adding shopify

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// navbar btn
 let barBtn = document.querySelector(".bar-btn")
 let navBar = document.querySelector(".navbar")
 let isActive = false
@@ -20,4 +21,39 @@ barBtn.addEventListener("click", ()=> {
         icon.classList.add("fa-bars")
         icon.classList.remove("fa-xmark")
     }
+})
+
+// project btn
+
+let codeBtn = document.querySelector('.codeBtn')
+let cmsBtn = document.querySelector('.cmsBtn')
+
+let code = document.querySelectorAll('.code')
+let cms = document.querySelector('.cms')
+
+codeBtn.addEventListener('click', () =>{
+    codeBtn.classList.add('active')
+    codeBtn.classList.remove('hide')
+    cmsBtn.classList.add('remove')
+    cmsBtn.classList.remove('active')
+    code.forEach((i) => {
+        i.classList.add('active')
+        i.classList.remove('hide')
+        cms.classList.add('hide')
+        cms.classList.remove('active')
+    })
+    
+})
+
+cmsBtn.addEventListener('click', () =>{
+    cmsBtn.classList.add('active')
+    cmsBtn.classList.remove('hide')
+    codeBtn.classList.add('remove')
+    codeBtn.classList.remove('active')
+    code.forEach((i) => {
+        cms.classList.add('active')
+        cms.classList.remove('hide')
+        i.classList.add('hide')
+        i.classList.remove('active')
+    })  
 })
